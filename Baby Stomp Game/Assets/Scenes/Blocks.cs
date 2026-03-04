c3523238f79aabad813ba990a7d78716b64bfbae
JusnWasHere <jcowling3000@gmail.com>
2026-03-04 09:46:33 -0500

    deleted unnecessary code

--- Baby Stomp Game/Assets/Scenes/Blocks.cs
+++ Baby Stomp Game/Assets/Scenes/Blocks.cs
@@ -2,36 +2,17 @@ using UnityEngine;
 
 public class Blocks : MonoBehaviour
 {
-    Foot foot;
-    ToddlerScript toddler;
-    float x;
-    float y;
-    float z;
-    Vector3 position;
     Rigidbody rb;
-    //public Rigidbody leftFootRB;
-    //public Rigidbody rightFootRB;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
-
-
     }
-
-
     void Update()
     {
-        //blockdestroy();
+        
     }
     public void blockdestroy()
     {
         Destroy(gameObject);
     }
-    //small blocks code
-
-
-
 }
