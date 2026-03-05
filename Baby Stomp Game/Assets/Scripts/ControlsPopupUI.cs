cdb9ba88c74ab5dd7f5943b2ad126b6022898be7
JusnWasHere <jcowling3000@gmail.com>
2026-03-05 14:54:50 -0500

    Update ControlsPopupUI.cs

--- Baby Stomp Game/Assets/Scripts/ControlsPopupUI.cs
+++ Baby Stomp Game/Assets/Scripts/ControlsPopupUI.cs
@@ -4,9 +4,14 @@ public class ControlsPopupUI : MonoBehaviour
 {
     public GameObject popup;
 
+    void start()
+    {
+        Time.timeScale = 0f;
+    }
     public void ClosePopup()
     {
         popup.SetActive(false);
+        Time.timeScale = 1f;
     }
 
 
